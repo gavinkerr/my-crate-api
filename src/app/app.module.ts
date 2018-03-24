dd0e2d259e45356d672a0ec9a11f2f8827c409d4
Gavin kerr <gavinkerr@Gavins-MacBook-Air.local>
2018-03-24 14:09:28 +0800

    set up and header

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { RecordListComponent } from './record-list/record-list.component';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecordListComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
